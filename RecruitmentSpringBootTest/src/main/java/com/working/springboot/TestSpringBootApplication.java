fa78f09ee8ff7ea3c1d7a4204b479c84827d573b
hemantnaag <79200867+hemantnaag@users.noreply.github.com>
2021-04-03 14:00:39 +0530

    Add files via upload

---
+++ RecruitmentSpringBootTest/src/main/java/com/working/springboot/TestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.working.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootApplication.class, args);
+	}
+
+}
---
+++ RecruitmentSpringBootTest/src/test/java/com/working/testSpringBoot/TestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.working.testSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
